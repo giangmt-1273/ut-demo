8dfd03a95038949713779901fa5a2a53b0d1293c
giangmt-1273 <mai.thi.giang@sun-asterisk.com>
2019-08-01 09:27:25 +0700

    Initial Commit

---
+++ UT-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UT-Demo
+//
+//  Created by maithigiang on 8/1/19.
+//  Copyright © 2019 maithigiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
